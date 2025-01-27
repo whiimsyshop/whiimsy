e9f32c7573c0ca5c3ed720eeabf90200c44337a8
singhmohit7057 <singhmohit7057@gmail.com>
2025-01-28 01:31:05 +0530

    sitemap & robots

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,17 +8,47 @@ export default function sitemap(): MetadataRoute.Sitemap {
         changeFrequency: "monthly",
         priority: 0.9,
       },
+      {
+        url: "https://whiimsy.in/services",
+        lastModified: new Date(),
+        changeFrequency: "weekly",
+        priority: 0.9,
+      },
     {
       url: "https://whiimsy.in/about",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
-    },npn
+    },
     {
       url: "https://whiimsy.in/contact-us",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.7,
+    },
+    {
+      url: "https://whiimsy.in/free-candle",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
+      url: "https://whiimsy.in/privacy-policy",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "https://whiimsy.in/cookies",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "https://whiimsy.in/terms-and-conditions",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
     },
     
   ];
