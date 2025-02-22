701c3159a7be00970b67bc01d702beef38b3f3bf
singhmohit7057 <singhmohit7057@gmail.com>
2025-02-22 14:36:46 +0530

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import "./globals.css"; // Ensure styles are applied
+import "./not-found.css"; // Ensure styles are applied
 
 export default function NotFound() {
   return (
