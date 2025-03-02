d610d93fa1c0c49b728a1d176b973e6c6e3dc140
singhmohit7057 <singhmohit7057@gmail.com>
2025-03-02 19:34:40 +0530

    fixll

--- src/components/AnalyticsTracker.tsx
+++ src/components/AnalyticsTracker.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect } from "react";
 import { usePathname } from "next/navigation";
 import { useRouter } from "next/router";  // Import useRouter instead of usePathname
