f78cd55867e965eddb0483ed42f48540fe995edc
singhmohit7057 <singhmohit7057@gmail.com>
2025-03-02 15:01:06 +0530

    fix

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -3,7 +3,7 @@ import ImageGallery from "@/components/ImageGallery";
 const Gallery = () => {
   return (
     <div className="container mx-auto p-6">
-      <ImageGallery priority={true} loading="eager" />
+      <ImageGallery />
     </div>
   );
 };
